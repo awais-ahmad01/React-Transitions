375c9763a0735ab4a502dda4587e69ddf7cc0205
awais-ahmad01 <aahmad19376@gmail.com>
2024-07-13 16:00:25 -0700

    Completed Transition Module

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/header';
 import CssTr from './components/CSStransition';
 import Tgroup from './components/Tgroup';
 import TransitionComp from './components/Transition';
+import Other from './components/other';
 
 const App = () => {
   return(
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/transition" element={<TransitionComp/>}></Route>
           <Route path="/csstransition" element={<CssTr/>}></Route>
           <Route path="/tgroup" element={<Tgroup/>}></Route>
+          <Route path="/other" element={<Other/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
