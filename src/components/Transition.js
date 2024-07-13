eec5c997c29fd80aa065e7c19446846e5b5319dc
awais-ahmad01 <aahmad19376@gmail.com>
2024-07-13 16:00:25 -0700

    Completed Transition Module

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,5 +1,62 @@
+
+import { useState } from "react";
+import { Transition } from 'react-transition-group'
+
+
 const TransitionComp = () => {
-  return <div>Transition</div>;
+  let [show,setShow] = useState(true)
+
+  const toggleDiv = () =>{
+    setShow(!show ? true :false)
+  }
+
+
+  return (
+    <>
+
+      {/* react transition use states and css transitions */}
+      {/* react transition give us on entering or leaving state and then we need to define our own styles/logic */}
+
+      {/* <Transition
+        in={show}
+        timeout={2000}
+      >  
+      { state => 
+        <div style={{
+          background:'red',
+          height:'100px',
+          transition:'all 2s ease',
+          opacity: state === 'exited' || state === 'exiting' ? 0:1
+        }}>
+          {state}
+        </div>
+      }
+      </Transition> */}
+
+      <Transition
+        in={show}
+        timeout={{
+          enter:2000,
+          exit:50
+        }}
+        // enter={false} // removes the entering
+        // exit={false} // removes the exiting
+        onEnter={(node)=>{
+          console.log('ENTER')
+        }}
+        onExit={(node)=>{
+          console.log('EXIT')
+        }}
+      >  
+        { state => 
+          <div className={`square square-${state}`}>
+            {`square square-${state}`}
+          </div>
+        }
+      </Transition>
+      <button className="btn btn-primary" onClick={toggleDiv}>Toggle show</button>
+    </>
+  );
 };
 
 export default TransitionComp;
---
+++ src/components/other.js
@@ -0,0 +1,32 @@
+
+import 'animate.css';   //css library provides built in transitions 
+import { useState } from 'react';
+
+const Other = () => {
+    const [anim,setAnim] = useState(true);
+
+    const handleAnim = () => {
+        if(anim){
+            return 'animate__backInLeft'
+        }
+        return 'animate__backOutLeft'
+    }
+
+    const toggleAnim = () =>{
+        setAnim(!anim)
+    }
+
+    return(
+        <>
+            <h1 
+            className={`animate__animated ${handleAnim()}`}>Transition</h1>
+            <button
+                onClick={toggleAnim}
+            >
+                Toggle anim
+            </button>
+        </>
+    )
+}
+
+export default Other;
